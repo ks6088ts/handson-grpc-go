17071cdf612b5c251667350cbe303a75b7e73a84
ks6088ts <ks6088ts@gmail.com>
2022-06-15 18:44:45 +0900

    implement helloworld client

--- cmd/helloworld_client.go
+++ cmd/helloworld_client.go
@@ -22,9 +22,24 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"flag"
+	"log"
+	"time"
 
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	helloworldClientDefaultName = "world"
+)
+
+var (
+	helloworldClientAddr = flag.String("addr", "localhost:50051", "the address to connect to")
+	helloworldClientName = flag.String("name", helloworldClientDefaultName, "Name to greet")
 )
 
 // clientCmd represents the client command
@@ -38,7 +53,23 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("client called")
+		flag.Parse()
+		// Set up a connection to the server.
+		conn, err := grpc.Dial(*helloworldClientAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		defer conn.Close()
+		c := pb.NewGreeterClient(conn)
+
+		// Contact the server and print out its response.
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *helloworldClientName})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.GetMessage())
 	},
 }
 
