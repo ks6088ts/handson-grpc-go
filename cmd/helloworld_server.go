34686362dcd933d270c0cb2f63eccf850e9989ad
ks6088ts <ks6088ts@gmail.com>
2022-06-15 18:41:09 +0900

    implement helloworld server

--- cmd/helloworld_server.go
+++ cmd/helloworld_server.go
@@ -22,10 +22,31 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
 
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
+
+var (
+	helloworldServerPort = flag.Int("port", 50051, "The server port")
+)
+
+// helloWorldServer is used to implement helloworld.GreeterServer.
+type helloWorldServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *helloWorldServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
@@ -38,7 +59,17 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("server called")
+		flag.Parse()
+		lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *helloworldServerPort))
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+		pb.RegisterGreeterServer(s, &helloWorldServer{})
+		log.Printf("server listening at %v", lis.Addr())
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
 	},
 }
 
